00a0232af25781485157e23e98cb650b00b0f7c4
dbotti <davide.botti@lastminute.com>
2022-06-17 12:32:53 +0200

    Adds styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
 import { Name, Name2, Name3 } from './Name';
+import styled from "styled-components";
+
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 function App() {
   return (
     <div className="App">
+        <Title>Title</Title>
       <Name name='Davide' surname='Botti'/>
       <Name2 name='Davide' surname='Botti'/>
       <Name3 name='Davide' surname='Botti'/>
