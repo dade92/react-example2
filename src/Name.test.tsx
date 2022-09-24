8b253bdf3e5158875f739ed65c666516a27f5ee4
davide <davidebotti92@gmail.com>
2022-09-24 10:54:06 +0200

    Add mock callback to test

--- src/Name.test.tsx
+++ src/Name.test.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Name } from "/Users/davide/Documents/programming/react-example2/src/Name"
 
 describe('Name', () => {
   it('contains correct properties', () => {
-    const {getByTestId} = render(<Name name={'ciccio'} surname={'pasticcio'} onClick={jest.fn} />);
-    expect(getByTestId('name')).toHaveTextContent("Name: ciccio Surname: pasticcio");
+    const callback = jest.fn();
+    render(<Name name={'ciccio'} surname={'pasticcio'} onClick={callback} />);
+    expect(screen.getByTestId('name')).toHaveTextContent("Name: ciccio Surname: pasticcio");
+
+    fireEvent.click(screen.getByTestId('name'));
+    fireEvent.click(screen.getByTestId('name'));
+    expect(callback.mock.calls.length).toBe(2);
   });
   
 })
