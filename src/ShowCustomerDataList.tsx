8f9143dbb01555b83dfce8979f9c8e56ffa0b369
dbotti <davide.botti@lastminute.com>
2022-10-10 09:19:01 +0200

    Refactor

--- src/ShowCustomerDataList.tsx
+++ src/ShowCustomerDataList.tsx
@@ -1,10 +1,10 @@
-import { Box, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
-import React, { useCallback, useEffect, useState } from "react";
-import { RemoteUser } from "./Data";
+import {Box, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText} from "@mui/material";
+import React, {useCallback, useEffect, useState} from "react";
+import {RemoteUser} from "./Data";
 import CommentIcon from '@mui/icons-material/Comment';
 import InboxIcon from '@mui/icons-material/Inbox';
 import DraftsIcon from '@mui/icons-material/Drafts';
-import { adaptUsers } from "./RemoteUserResponseAdapter";
+import {adaptUsers} from "./RemoteUserResponseAdapter";
 
 enum Action {
     INBOX = "INBOX",
@@ -12,16 +12,18 @@ enum Action {
 }
 
 export const ShowCustomerDataList: React.FC = () => {
-    const [users,setUsers] = useState<RemoteUser[]>([])
+    const [users, setUsers] = useState<RemoteUser[]>([])
 
     const fetchData = useCallback(async () => {
         const data = await fetch('http://localhost:8081/retrieveUsers');
         const response = await data.json();
         console.log(response);
         setUsers(adaptUsers(response.users));
-      },[]);
-    
-    useEffect(()=> { fetchData() }, [fetchData])
+    }, []);
+
+    useEffect(() => {
+        fetchData()
+    }, [fetchData])
 
     const handleComment = (userName: string) => {
         console.log(userName);
@@ -32,41 +34,41 @@ export const ShowCustomerDataList: React.FC = () => {
     }
 
     return (
-        <Box sx={{ width: '100%', maxWidth: 360, overflow: 'hidden', border: '1px dashed grey' }}>
+        <Box sx={{width: '100%', maxWidth: 360, overflow: 'hidden', border: '1px dashed grey'}}>
             <List>
-                {users.map(user => {
-                    return <ListItem 
-                            key={user.name} 
-                            divider={true}
-                            secondaryAction={
-                                <IconButton aria-label="comment" onClick={()=>handleComment(user.name)}>
-                                    <CommentIcon />
-                                </IconButton>}
-                            >
-                            <ListItemText>
-                                {user.name} - {user.surname} - {user.data.profile}
-                            </ListItemText>
+                {users.map(user =>
+                    <ListItem
+                        key={user.name}
+                        divider={true}
+                        secondaryAction={
+                            <IconButton aria-label="comment" onClick={() => handleComment(user.name)}>
+                                <CommentIcon/>
+                            </IconButton>}
+                    >
+                        <ListItemText>
+                            {user.name} - {user.surname} - {user.data.profile}
+                        </ListItemText>
                     </ListItem>
-                })}
+                )}
             </List>
             <List>
-          <ListItem disablePadding>
-            <ListItemButton onClick={()=>handleClick(Action.INBOX)}>
-              <ListItemIcon>
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary="Inbox" />
-            </ListItemButton>
-          </ListItem>
-          <ListItem disablePadding>
-            <ListItemButton onClick={()=>handleClick(Action.DRAFTS)}>
-              <ListItemIcon>
-                <DraftsIcon />
-              </ListItemIcon>
-              <ListItemText primary="Drafts" />
-            </ListItemButton>
-          </ListItem>
-        </List>
+                <ListItem disablePadding>
+                    <ListItemButton onClick={() => handleClick(Action.INBOX)}>
+                        <ListItemIcon>
+                            <InboxIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary="Inbox"/>
+                    </ListItemButton>
+                </ListItem>
+                <ListItem disablePadding>
+                    <ListItemButton onClick={() => handleClick(Action.DRAFTS)}>
+                        <ListItemIcon>
+                            <DraftsIcon/>
+                        </ListItemIcon>
+                        <ListItemText primary="Drafts"/>
+                    </ListItemButton>
+                </ListItem>
+            </List>
         </Box>
-  );
+    );
 }
