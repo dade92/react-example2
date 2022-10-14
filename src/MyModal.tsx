d050dc60fc3293b48d841ec49fddc55008cb554d
davide <davidebotti92@gmail.com>
2022-10-14 16:47:06 +0200

    Try some layout stuff with dialog

--- src/MyModal.tsx
+++ src/MyModal.tsx
@@ -4,18 +4,18 @@ import CloseIcon from '@mui/icons-material/Close';
 
 interface Props {
     isOpen: boolean;
-    onClose: ()=>void;
     onConfirm: ()=>void;
+    onClose: ()=>void;
 }
 
 export const MyModal: FC<Props> = ({isOpen, onClose, onConfirm}) => {
     return (
         <Dialog
+        style={{color: 'black'}}
         open={isOpen}
         onClose={onClose}
         aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
+        aria-describedby="alert-dialog-description">
         <DialogTitle id="alert-dialog-title">
           {"Are you sure?"}
           {
@@ -32,7 +32,7 @@ export const MyModal: FC<Props> = ({isOpen, onClose, onConfirm}) => {
               </IconButton>
           }
         </DialogTitle>
-        <DialogContent>
+        <DialogContent dividers>
           <DialogContentText id="alert-dialog-description">
             By clicking on confirm you confirm the operation
           </DialogContentText>
