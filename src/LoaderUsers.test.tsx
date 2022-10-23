cacd6e98496f64ca7571b8f14eb283ce61701638
davide <davidebotti92@gmail.com>
2022-10-23 15:09:21 +0200

    Adds some tests

---
+++ src/LoaderUsers.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react"
+import { LoaderUsers } from "./LoaderUsers"
+
+describe('LoaderUsers',()=> {
+
+    it('renders loader in case of no errors', ()=>{
+        render(<LoaderUsers error={false}/>);
+
+        expect(screen.getByTestId('loader')).toBeDefined();
+        expect(screen.queryByTestId('error-label')).toBeNull();
+    })
+
+    it('renders error message in case of errors', ()=>{
+        render(<LoaderUsers error={true}/>);
+
+        expect(screen.queryByTestId('loader')).toBeNull();
+        expect(screen.getByTestId('error-label')).toBeDefined();
+    })
+})
