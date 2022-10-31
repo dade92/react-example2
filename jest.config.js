d9b9dc404d05b865f33b975f2dc70ea3703974f5
davide <davidebotti92@gmail.com>
2022-10-31 09:22:07 +0100

    Adds jest-preview config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  transform: {
+    "^.+\\.(css|scss|sass|less)$": "jest-preview/transforms/css",
+    "^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)": "jest-preview/transforms/file",
+  },
   globals: {
     appVersion: '0.0.0',
   },
