29611c6f67030061259fcef74034f16cdb541dfb
dbotti <davide.botti@lastminute.com>
2022-11-03 09:51:44 +0100

    Remove useless code

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import styled from 'styled-components';
 import {AppFlow} from './AppFlow';
 import "./App.css";
 
-const AppFlowContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`
-
 const App: React.FC = () => (
     <div className='AppFlow'>
         <AppFlow/>
