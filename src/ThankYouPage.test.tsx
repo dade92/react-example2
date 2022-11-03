ae938db76aef60ae703d102a3bec55ac98de2258
dbotti <davide.botti@lastminute.com>
2022-11-03 23:04:13 +0100

    Adds test

--- src/ThankYouPage.test.tsx
+++ src/ThankYouPage.test.tsx
@@ -1,11 +1,15 @@
-import {render, screen} from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 import {ThankYouPage} from './ThankYouPage'
 
 describe('ThankYouPage', () => {
     it('renders correctly', () => {
-        render(<ThankYouPage customerName={'Sergio'} onRestart={jest.fn}/>);
+        let callback = jest.fn();
+        render(<ThankYouPage customerName={'Sergio'} onRestart={callback}/>);
+
+        fireEvent.click(screen.getByTestId('restart-button'))
 
         expect(screen.getByTestId('thankyou-message').textContent).toBe('Thanks for your selection Sergio!');
         expect(screen.getByTestId('thumbs-up')).toBeVisible();
+        expect(callback).toHaveBeenCalledTimes(1);
     })
 })
