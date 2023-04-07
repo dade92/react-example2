2c0b84cb36ab97465aa7135aa48a4eb4d4a61cf5
dbotti <davide.botti@lastminute.com>
2023-04-07 09:18:12 +0200

    Reformat

--- src/RestClientConfiguration.tsx
+++ src/RestClientConfiguration.tsx
@@ -7,7 +7,6 @@ if (process.env.REACT_APP_STAGE === 'production') {
     host = 'http://localhost:8080'
 }
 
-
 const RestClientContext = React.createContext<RestClient>(new RestClient(host));
 
 export const RestClientConfiguration: React.FC<{ children: ReactNode }> = ({children}) => {
