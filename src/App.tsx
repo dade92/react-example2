25050af07cefcff6d65df57447b6bfbb39067edc
dbotti <davide.botti@lastminute.com>
2023-05-12 17:37:45 +0200

    Fist POC for web sockets

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {AppFlow} from './AppFlow';
 import "./App.css";
 import RestClientConfiguration from "./RestClientConfiguration";
-import { TranslationsConfiguration } from './TranslationsConfiguration';
+import {TranslationsConfiguration} from './TranslationsConfiguration';
+import SockJsClient from 'react-stomp';
 
-const App: React.FC = () => (
-    <div className='AppFlow'>
-        <RestClientConfiguration>
-            <TranslationsConfiguration>
+const SOCKET_URL = '/ws-message';
+
+interface TextMessageDTO {
+    message: string;
+}
+
+const App: React.FC = () => {
+
+    const [message, setMessage] = useState('You server message here.');
+
+    let onConnected = () => {
+        console.log("Connected!!")
+    }
+
+    let onMessageReceived = (msg: TextMessageDTO) => {
+        console.log(`Message received: ${msg.message}`)
+        setMessage(msg.message);
+    }
+
+
+    return (
+        <div className='AppFlow'>
+            <SockJsClient
+                url={SOCKET_URL}
+                topics={['/topic/message']}
+                onConnect={onConnected}
+                onDisconnect={console.log("Disconnected!")}
+                onMessage={(msg: TextMessageDTO) => onMessageReceived(msg)}
+                debug={false}
+            />
+            <span>{message}</span>
+            <RestClientConfiguration>
+                <TranslationsConfiguration>
                     <AppFlow/>
-            </TranslationsConfiguration>
-        </RestClientConfiguration>
-    </div>
-)
+                </TranslationsConfiguration>
+            </RestClientConfiguration>
+        </div>
+    );
+}
 
 export default App;
