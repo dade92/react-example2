9ceff6cebc1769e2a0d38a200439b2e0a43d6d4d
davide <davidebotti92@gmail.com>
2023-05-13 10:15:25 +0200

    Adds NOtificationManager plus experiment with react-stomp-hooks

--- src/App.tsx
+++ src/App.tsx
@@ -3,46 +3,41 @@ import {AppFlow} from './AppFlow';
 import "./App.css";
 import RestClientConfiguration from "./RestClientConfiguration";
 import {TranslationsConfiguration} from './TranslationsConfiguration';
-import SockJsClient from 'react-stomp';
-
-const SOCKET_URL = '/ws-message';
-
-interface TextMessageDTO {
-    message: string;
-}
+import { NotificationManager } from './NotificationManager';
+import {
+    StompSessionProvider,
+    useSubscription,
+  } from "react-stomp-hooks";
 
 const App: React.FC = () => {
-
-    const [message, setMessage] = useState('You server message here.');
-
-    let onConnected = () => {
-        console.log("Connected!!")
-    }
-
-    let onMessageReceived = (msg: TextMessageDTO) => {
-        console.log(`Message received: ${msg.message}`)
-        setMessage(msg.message);
-    }
-
-
     return (
         <div className='AppFlow'>
-            <SockJsClient
-                url={SOCKET_URL}
-                topics={['/topic/message']}
-                onConnect={onConnected}
-                onDisconnect={console.log("Disconnected!")}
-                onMessage={(msg: TextMessageDTO) => onMessageReceived(msg)}
-                debug={false}
-            />
-            <span>{message}</span>
+            <StompSessionProvider
+                url={"/ws-message"}
+                //All options supported by @stomp/stompjs can be used here
+    >
+        <SubscribingComponent />
             <RestClientConfiguration>
                 <TranslationsConfiguration>
                     <AppFlow/>
                 </TranslationsConfiguration>
             </RestClientConfiguration>
+            </StompSessionProvider>
         </div>
     );
 }
 
+const SubscribingComponent:React.FC = () => {
+    const [lastMessage, setLastMessage] = useState("No message received yet");
+
+    //Subscribe to /topic/test, and use handler for all received messages
+    //Note that all subscriptions made through the library are automatically removed when their owning component gets unmounted.
+    //If the STOMP connection itself is lost they are however restored on reconnect.
+    //You can also supply an array as the first parameter, which will subscribe to all destinations in the array
+    useSubscription("/topic/message", (message) => setLastMessage(message.body));
+  
+    return (
+      <div>Last Message: {lastMessage}</div>
+    );
+}
 export default App;
