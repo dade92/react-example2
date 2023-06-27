e0be22a22c049af076cfec1a5efe44697c290783
dbotti <davide.botti@lastminute.com>
2023-06-27 20:28:39 +0200

    Fix problem with web socket in local

--- src/SubscriberComponent.tsx
+++ src/SubscriberComponent.tsx
@@ -4,9 +4,8 @@ import {useSubscription} from "react-stomp-hooks";
 import {TextMessage} from "./NotificationManager";
 
 
-export const ConditionalSubscriber: React.FC = () => {
-    return process.env.REACT_APP_STAGE === 'local' ? null : <SubscriberComponent/>;
-}
+export const ConditionalSubscriber: React.FC = () =>
+    process.env.REACT_APP_STAGE === 'local' ? null : <SubscriberComponent/>
 
 export const SubscriberComponent: React.FC = () => {
     const [lastMessage, setLastMessage] = useState<string | null>(null);
