720b230f861a867c2eac1d95d9a87087f64b55d4
Davide Botti <davidebotti92@gmail.com>
2023-08-13 21:55:24 +0200

    Restyling

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -15,7 +15,32 @@ export const MyCarousel: React.FC = () => {
     ]
 
     return (
-        <Carousel>
+        <Carousel 
+            next={ () => {/* Do stuff */} }
+            prev={ () => {/* Do other stuff */} }
+            indicatorIconButtonProps={{
+                style: {
+                    color: 'grey'       // 3
+                }
+            }}
+            activeIndicatorIconButtonProps={{
+                style: {
+                    backgroundColor: 'white' // 2
+                }
+            }}
+            indicatorContainerProps={{
+                style: {
+                    marginTop: '50px', // 5
+                }
+        
+            }}
+            fullHeightHover={false}
+            navButtonsProps={{          // Change the colors and radius of the actual buttons. THIS STYLES BOTH BUTTONS
+                style: {
+                    borderRadius: 8
+                }
+            }} 
+        >
             {
                 items.map( (item, i) => <CarouselItem key={i} name={item.name} description={item.description} /> )
             }
