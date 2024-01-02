ab05b3cc4de208b2faeea69dd5693ac24be4ba9f
Davide Botti <davidebotti92@gmail.com>
2024-01-02 22:18:33 +0100

    On deploy, starts only FE and reverse -proxy

---
+++ deploy/run.sh
@@ -0,0 +1,21 @@
+echo "Pulling the latest images..."
+docker compose pull frontend
+docker compose pull proxy
+
+echo "Starting frontend app..."
+docker compose up -d frontend
+sleep 2
+
+#echo "Starting mongo express interface on port 8081..."
+#docker compose up -d mongo-express
+#sleep 2
+
+echo "Starting nginx reverse proxy listening on port 80..."
+docker compose up -d proxy
+sleep 4
+
+docker ps
+
+echo "App seems up and running. You can access at http://localhost"
+
+
